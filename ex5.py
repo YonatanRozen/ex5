97bfdf10209f1eaa99bb16a58c735ac2bb0fed76
YonatanRozen <rosenyo2912@gmail.com>
2022-06-27 22:52:37 +0300

    Finishing up...

--- ex5.py
+++ ex5.py
@@ -7,64 +7,98 @@ def names_of_registered_students(input_json_path, course_name):
     # Opening JSON file
     file = open(input_json_path)
 
-    registered_students = []
-
-    # Parsing the data from the JSON file
-    # Returns a dictionary
+    # Parsing data
     data = json.load(file)
+    key_list = list(data.keys())
 
-    for name, courses in zip(data["student_name"], data["registered_courses"]):
-        if course_name in courses:
-            registered_students.append(name)
+    # Creating list of names...
+    course_list = []
+    for key in key_list:
+        if course_name in data[key]["registered_courses"]:
+            course_list.append(data[key]["student_name"])
+        else:
+            continue
 
     file.close()
-    return registered_students
+    return course_list
 
 
 def enrollment_numbers(input_json_path, output_file_path):
     # Opening JSON file
-    file = open(input_json_path)
+    in_file = open(input_json_path, 'r')
+    out_file = open(output_file_path, 'w')
 
-    # Parsing the data from the JSON file
-    # Returns a dictionary
-    data = json.load(file)
+    # Parsing data
+    data = json.load(in_file)
+    keys_json = list(data.keys())
 
-    courses_dict = get_dict_of_courses(data)
-    for student_courses in data["registered_courses"]:
-        update_enrollment_numbers(student_courses, courses_dict)
+    dic = get_student_course_dict(input_json_path, data, keys_json)
+    keys_courses = sorted(list(dic.keys()))
+    for key in keys_courses:
+        out_file.write('"{}" {}{}'.format(key, dic[key], '\n'))
 
-    key_list = list(courses_dict.keys())
-    val_list = list(courses_dict.values())
-    for course_data in courses_dict:
-        pos = val_list.index(course_data)
-        json.dump(key_list[pos] + ' ' + course_data + '\n', output_file_path, indent=4)
-    file.close()
+    in_file.close()
+    out_file.close()
 
 
 def courses_for_lecturers(json_directory_path, output_json_path):
-    """
-    This function writes the courses given by each lecturer in json format.
+    in_files = get_json_files(json_directory_path)
+    out_file = open(output_json_path, 'w')
+    all_lecturers = {}
+    for filename in in_files:
+        file = open(filename, 'r')
+        data = json.load(file)
+        all_lecturers.update(get_all_lecturers(data))
+        keys = list(all_lecturers.keys())
+        for key in keys:
+            info = get_lecturer_course_list(data, key)
+            for course in info:
+                if course not in all_lecturers[key]:
+                    all_lecturers[key].append(course)
+        file.close()
+    json.dump(all_lecturers, out_file, indent=4)
+    out_file.close()
 
-    :param json_directory_path: Path of the semsters_data files.
-    :param output_json_path: Path of the output json file.
-    """
-    pass
+
+def get_all_lecturers(data):
+    lecturers = {}
+    keys = list(data.keys())
+    for key in keys:
+        for lecturer in data[key]['lecturers']:
+            if lecturer not in lecturers:
+                lecturers[lecturer] = []
+            else:
+                continue
+    return lecturers
 
 
-def update_enrollment_numbers(registered_courses, courses_dict):
-    for course in registered_courses:
-        courses_dict[course] += 1
+def get_lecturer_course_list(data, lecturer):
+    courses = []
+    keys = list(data.keys())
+    for key in keys:
+        if lecturer in data[key]['lecturers'] and data[key]['course_name'] not in courses:
+            courses.append(data[key]['course_name'])
+    return courses
 
 
-def get_dict_of_courses(data):
-    course_list = []
-    for courses in data["registered_courses"]:
-        for item in courses:
-            if item not in course_list:
-                course_list.append(item)
-    course_list.sort()
+def get_student_course_dict(json_file_path, data, keys):
+    dic = {}
+    for key in keys:
+        for item in data[key]["registered_courses"]:
+            if item not in dic:
+                students_registered = names_of_registered_students(json_file_path, item)
+                amount = len(students_registered)
+                dic[item] = amount
+            else:
+                continue
+    return dic
 
-    course_dict = {}
-    for item in course_list:
-        course_dict[item] = 0
-    return course_dict
+
+def get_json_files(json_directory_path):
+    files = []
+    for file in os.listdir(json_directory_path):
+        if file.endswith('.json'):
+            files.append(json_directory_path + "\\" + file)
+        else:
+            continue
+    return files
