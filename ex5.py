126712574ada4fc7bd236e5ef93db5ccdd4842db
liormalka261 <102741933+liormalka261@users.noreply.github.com>
2022-06-28 21:19:27 +0300

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -46,7 +46,7 @@ def courses_for_lecturers(json_directory_path, output_json_path):
     dict_lecturers = {}
     for file in files:
         if file.endswith(".json"):
-            current_file = open(json_directory_path + "\\" + file, "r")
+            current_file = open(json_directory_path + "/" + file, "r")
             data = json.load(current_file)
             for course in data:
                 for lecturer in data[course]["lecturers"]:
@@ -61,27 +61,6 @@ def courses_for_lecturers(json_directory_path, output_json_path):
     output_file.close()
 
 
-def get_all_lecturers(data):
-    lecturers = {}
-    keys = list(data.keys())
-    for key in keys:
-        for lecturer in data[key]['lecturers']:
-            if lecturer not in lecturers:
-                lecturers[lecturer] = []
-            else:
-                continue
-    return lecturers
-
-
-def get_lecturer_course_list(data, lecturer):
-    courses = []
-    keys = list(data.keys())
-    for key in keys:
-        if lecturer in data[key]['lecturers'] and data[key]['course_name'] not in courses:
-            courses.append(data[key]['course_name'])
-    return courses
-
-
 def get_student_course_dict(json_file_path, data, keys):
     dic = {}
     for key in keys:
@@ -93,13 +72,3 @@ def get_student_course_dict(json_file_path, data, keys):
             else:
                 continue
     return dic
-
-
-def get_json_files(json_directory_path):
-    files = []
-    for file in os.listdir(json_directory_path):
-        if file.endswith('.json'):
-            files.append(json_directory_path + "\\" + file)
-        else:
-            continue
-    return files
